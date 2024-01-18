58f03be0ca381463cd2e15a30ff5dac7f900b72c
Nicolas Parrella <nparrella@ataway.com>
2024-01-17 21:02:14 -0300

    Add fondo component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        tomaso: ["Tomaso", "sans-serif"],
+        jura: ["Jura", "sans-serif"],
+      },
+      colors: {
+        primary: "rgb(253 224 71);",
+      },
+    },
+  },
+  plugins: [],
+};
